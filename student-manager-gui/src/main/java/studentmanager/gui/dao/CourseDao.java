abeb41968050c888dc0cf378db89bb36e2e04bb8
tongxiangwang1 <leonwang233@gmail.com>
2025-11-02 23:48:41 +1300

    Perform simple GUI enhancements

--- student-manager-gui/src/main/java/studentmanager/gui/dao/CourseDao.java
+++ student-manager-gui/src/main/java/studentmanager/gui/dao/CourseDao.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package studentmanager.gui.dao;
-
+//
 import studentmanager.gui.db.DatabaseManager;
 import studentmanager.gui.model.Course;
 
